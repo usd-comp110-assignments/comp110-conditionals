0648b9b6367b972a0e32d6205abb3becd1ac900c
Jennifer Olsen <jenniferolsen@sandiego.edu>
2025-09-22 23:47:59 -0700

    initial starter code

---
+++ bubble_chart.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+def create_bubbles(canvas):
+    pass
+
+def get_color(value):
+    pass
+
+def main():
+    """
+    Main function 
+    """
+
+    root = tk.Tk()
+    root.title("USD COMP110 Bubble Chart")
+
+    canvas = tk.Canvas(root, width=500, height=500, bg="white")
+    canvas.pack()
+
+    create_bubbles(canvas)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
